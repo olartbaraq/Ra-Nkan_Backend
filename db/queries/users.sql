ea13295848b82f060ba15f186f1319bf34fc708d
Olartbaraq <akanbi.ges201@gmail.com>
2023-11-25 05:30:23 +0100

    added sqlc files

--- db/queries/users.sql
+++ db/queries/users.sql
@@ -8,21 +8,19 @@ INSERT INTO users (
     hashed_password,
     is_admin
 ) VALUES (
-    $1, $2, $3, $4, $5, $6) RETURNING *;
+    $1, $2, $3, $4, $5, $6, $7) RETURNING *;
 
--- $1, $2 must come sequentially as listed above
-
---name: GetUserById :one
+-- name: GetUserById :one
 SELECT * FROM users WHERE id = $1;
 
---name: GetUserByEmail :one
+-- name: GetUserByEmail :one
 SELECT * FROM users WHERE email = $1;
 
---name: GETALLUSER :many
+-- name: ListAllUsers :many
 SELECT * FROM users ORDER BY id LIMIT $1 OFFSET $2;
 
---name: UpdateUserPassword :one
+-- name: UpdateUserPassword :one
 UPDATE users SET hashed_password = $1 WHERE id = $2 RETURNING *;
 
---name: DeleteUser :exec
+-- name: DeleteUser :exec
 DELETE FROM users WHERE id = $1;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/olartbaraq/spectrumshelf
+
+go 1.21.4
